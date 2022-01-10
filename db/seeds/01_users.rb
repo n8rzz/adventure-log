be688023683956324051d35eb5414717fcfd910d
nate geslin <teamtomkins23@gmail.com>
2022-01-09 20:31:41 -0600

    adds other_user seeds for users and journal_entires

--- db/seeds/01_users.rb
+++ db/seeds/01_users.rb
@@ -2,14 +2,24 @@ puts '=== Users'
 
 password = 's@f3wo4d'
 
-# This use belongs to a group
 admin = User.create(
   email: 'me@example.com',
-  name: 'adventure admin',
+  name: Faker::Movies::Lebowski.character,
   password: password,
   password_confirmation: password,
   provider: 'google_oauth2',
   uid: '123456789'
+)
+
+print '.'
+
+manualUser = User.create(
+  email: 'you@example.com',
+  name: Faker::Movies::Lebowski.character,
+  password: password,
+  password_confirmation: password,
+  provider: 'google_oauth2',
+  uid: '2345678910'
 )
 
 print '.'
--- db/seeds/02_journal_entries.rb
+++ db/seeds/02_journal_entries.rb
@@ -13,4 +13,14 @@ rand(13..42).times do
   print '.'
 end
 
-print '.'
+other_user = User.find_by(email: 'you@example.com')
+
+rand(3..13).times do
+  JournalEntry.create(
+    body: Faker::Movies::StarWars.quote,
+    entry_date: Faker::Date.between(from: 2.months.ago, to: Date.today),
+    user: other_user
+  )
+
+  print '.'
+end
